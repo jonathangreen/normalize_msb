482bf19d34adda802301ea44d88e5dd5a6fbad7d
Christian Gudrian <christian.gudrian@gmx.de>
2010-02-26 22:57:48 +0100

    Added some files for backup purposes

---
+++ gettext.h
@@ -0,0 +1,17 @@
+#ifndef __GETTEXT_H__
+#define __GETTEXT_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static const char * gettext( const char * msg )
+{
+   return msg;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __GETTEXT_H__ */
