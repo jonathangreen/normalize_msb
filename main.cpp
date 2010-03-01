cc1df63726db60be2e8c6640275d3f3ad8bcd43f
Christian Gudrian <christian.gudrian@gmx.de>
2010-03-01 11:23:57 +0100

    GPL preparations

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 /*
- * This program sorts all items within an ItemGroup of an MSBuild file
- * first by its BuildOrder and then by its file name.  It has primarily been
- * developed to simplify version control of Embarcadero RAD Studio project
- * files.  With every save of the project file RAD Studio permutes the order
- * of the items randomly leading to unnecessary conflicts in case of a merge.
- *
- * This source code comes without any warranty.  Use at your own risk.
- *
- * Questions and comments to: christian.gudrian@gmx.de
- */
+   (c) Copyright 2010 Christian Gudrian
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <vcl.h>
 #pragma hdrstop
 
